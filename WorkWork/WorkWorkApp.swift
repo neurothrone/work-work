be8c37f4b9d10da6c05244330038e82d3b40ac27
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-01-02 06:44:34 +0100

    Initial Commit

---
+++ WorkWork/WorkWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkWorkApp.swift
+//  WorkWork
+//
+//  Created by Zaid Neurothrone on 2023-01-02.
+//
+
+import SwiftUI
+
+@main
+struct WorkWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

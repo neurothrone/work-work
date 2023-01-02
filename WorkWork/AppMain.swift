61c90118a333f99f535bd89b368463fc7752a5f8
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-01-02 08:32:26 +0100

    fix indentation and rename to AppMain

--- WorkWork/AppMain.swift
+++ WorkWork/AppMain.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 @main
-struct WorkWorkApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+struct AppMain: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }

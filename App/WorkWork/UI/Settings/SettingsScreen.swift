6b0b264e30d8d08cc30adb74a089ec9d16959cd1
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-01-03 09:48:06 +0100

    move all files to sub directory

---
+++ App/WorkWork/UI/Settings/SettingsScreen.swift
@@ -0,0 +1,28 @@
+//
+//  SettingsScreen.swift
+//  WorkWork
+//
+//  Created by Zaid Neurothrone on 2023-01-03.
+//
+
+import SwiftUI
+
+struct SettingsScreen: View {
+  var body: some View {
+    Form {
+      Text("Todo isDone style. Swipe only, Tap Only, Both")
+      
+      Text("Interactive color style. Selection of built-in dark/light mode supported colors. Start with 5-6 colors.")
+      
+      Text("List style. Inset Grouped or Grouped")
+    }
+  }
+}
+
+struct SettingsScreen_Previews: PreviewProvider {
+  static var previews: some View {
+    NavigationStack {
+      SettingsScreen()
+    }
+  }
+}

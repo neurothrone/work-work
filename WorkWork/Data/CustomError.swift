d12d62125c0c8378e86a78e0c573c9e1dec0f9de
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-01-03 03:12:12 +0100

    add custom errors for core data

---
+++ WorkWork/Data/CustomError.swift
@@ -0,0 +1,28 @@
+//
+//  CustomError.swift
+//  WorkWork
+//
+//  Created by Zaid Neurothrone on 2023-01-03.
+//
+
+import Foundation
+
+enum CustomError: Swift.Error, CustomLocalizedStringResourceConvertible {
+  case notFound(entityName: String),
+       coreDataSave,
+       unknownId(entityName: String, id: String),
+       unknownError(message: String),
+       deletionFailed(entityName: String),
+       addFailed(entityName: String)
+  
+  var localizedStringResource: LocalizedStringResource {
+    switch self {
+    case .addFailed(let entityName): return "❌ -> An error occurred trying to add \(entityName)"
+    case .deletionFailed(let entityName): return "❌ -> An error occurred trying to delete \(entityName)"
+    case .unknownError(let message): return "❌ -> An unknown error occurred: \(message)"
+    case .unknownId(let entityName, let id): return "❌ -> No \(entityName) with an ID matching: \(id)"
+    case .notFound(let entityName): return "❌ -> \(entityName) not found"
+    case .coreDataSave: return "❌ -> Failed to save to CoreData"
+    }
+  }
+}

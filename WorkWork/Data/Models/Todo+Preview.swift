cabd47df3f743471ae4c1506aac0967deed78801
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-01-03 07:09:46 +0100

    add preview code for init

---
+++ WorkWork/Data/Models/Todo+Preview.swift
@@ -0,0 +1,19 @@
+//
+//  Todo+Preview.swift
+//  WorkWork
+//
+//  Created by Zaid Neurothrone on 2023-01-03.
+//
+
+import CoreData
+
+extension Todo {
+  enum Preview {
+    static func generateSample(using context: NSManagedObjectContext) -> Todo {
+      let todo = Todo(context: context)
+      todo.title = "Preview Todo"
+      todo.save(using: context)
+      return todo
+    }
+  }
+}
--- WorkWork/Data/Models/TodoList+Preview.swift
+++ WorkWork/Data/Models/TodoList+Preview.swift
@@ -9,6 +9,20 @@ import CoreData
 
 extension TodoList {
   enum Preview {
+    static func generateSample(using context: NSManagedObjectContext) -> TodoList {
+      let todoList = TodoList(context: context)
+      todoList.name = "Preview List"
+      
+      for index in 1...5 {
+        let todo = Todo(context: context)
+        todo.title = "Preview Todo \(index)"
+        todo.list = todoList
+      }
+      
+      CoreDataProvider.save(using: context)
+      return todoList
+    }
+    
     static func generateSamples(using context: NSManagedObjectContext) {
       for index in 1...5 {
         let todoList = TodoList(context: context)

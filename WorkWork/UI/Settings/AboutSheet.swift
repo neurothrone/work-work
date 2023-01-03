e50841991def2ab92c47d19694bf5bf68a384ab8
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-01-03 09:04:41 +0100

    add placeholder sheets for settings screen

---
+++ WorkWork/UI/Settings/AboutSheet.swift
@@ -0,0 +1,20 @@
+//
+//  AboutSheet.swift
+//  WorkWork
+//
+//  Created by Zaid Neurothrone on 2023-01-03.
+//
+
+import SwiftUI
+
+struct AboutSheet: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+struct AboutSheet_Previews: PreviewProvider {
+  static var previews: some View {
+    AboutSheet()
+  }
+}
---
+++ WorkWork/UI/Settings/DeleteAllDataSheet.swift
@@ -0,0 +1,20 @@
+//
+//  DeleteAllDataSheet.swift
+//  WorkWork
+//
+//  Created by Zaid Neurothrone on 2023-01-03.
+//
+
+import SwiftUI
+
+struct DeleteAllDataSheet: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+struct DeleteAllDataSheet_Previews: PreviewProvider {
+  static var previews: some View {
+    DeleteAllDataSheet()
+  }
+}

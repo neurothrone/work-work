523ca1782ac620c6648e651b17793dd2663a8963
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-01-03 07:10:03 +0100

    add extensions to Todo and TodoList

---
+++ WorkWork/Data/Models/Todo+Extensions.swift
@@ -0,0 +1,32 @@
+//
+//  Todo+Extensions.swift
+//  WorkWork
+//
+//  Created by Zaid Neurothrone on 2023-01-03.
+//
+
+import CoreData
+
+extension Todo {
+  //MARK: - Requests
+  static func all(in list: TodoList) -> NSFetchRequest<Todo> {
+    let request = Todo.fetchRequest()
+    request.sortDescriptors = [.init(keyPath: \Todo.createdDate, ascending: false)]
+    request.predicate = NSPredicate(format: "%K == %@", "list.id", list.id as CVarArg)
+    return request
+  }
+  
+  //MARK: - Data
+  static func toggleIsDone(for todo: Todo, using context: NSManagedObjectContext) {
+    todo.isDone.toggle()
+    todo.save(using: context)
+  }
+  
+  
+  static func addTodo(with title: String, to list: TodoList, using context: NSManagedObjectContext) {
+    let todo = Todo(context: context)
+    todo.title = title
+    todo.list = list
+    todo.save(using: context)
+  }
+}
--- WorkWork/Data/Models/TodoList+Extensions.swift
+++ WorkWork/Data/Models/TodoList+Extensions.swift
@@ -7,11 +7,23 @@
 
 import CoreData
 
-//MARK: - Requests
 extension TodoList {
+  //MARK: - Requests
   static var all: NSFetchRequest<TodoList> {
     let request = TodoList.fetchRequest()
     request.sortDescriptors = [.init(keyPath: \TodoList.order, ascending: true)]
     return request
+  }
+  
+  //MARK: - Data
+  static func createWith(
+    name: String,
+    using context: NSManagedObjectContext
+  ) -> TodoList {
+    let todoList = TodoList(context: context)
+    todoList.name = name
+    todoList.save(using: context)
+    
+    return todoList
   }
 }

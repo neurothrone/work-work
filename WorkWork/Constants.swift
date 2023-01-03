78ca84b070e6f194f01bdda74bf871aa8fbb5821
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-01-03 03:12:29 +0100

    add constants

---
+++ WorkWork/Constants.swift
@@ -0,0 +1,29 @@
+//
+//  Constants.swift
+//  WorkWork
+//
+//  Created by Zaid Neurothrone on 2023-01-02.
+//
+
+enum MyApp {
+  static let name = "WorkWork"
+  
+  enum CKConfig {
+    static let containerName = "Entities"
+    static let sharedAppGroup = "group.workWork"
+    static let cloudContainerID = "iCloud.workWork"
+  }
+  
+  enum SystemImage {
+    static let settings = "gear"
+  }
+  
+  enum AppStorage {
+    static let selectedScreen = "selectedScreen"
+    static let isOnboardingPresented = true
+  }
+  
+  enum Link {
+    static let svgRepo = "https://www.svgrepo.com/"
+  }
+}

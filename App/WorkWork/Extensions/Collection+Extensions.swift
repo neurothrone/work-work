abcaa38fac836724b46db1270edf10a72143d725
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-01-05 17:11:13 +0100

    add helper property

---
+++ App/WorkWork/Extensions/Collection+Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  Collection+Extensions.swift
+//  WorkWork
+//
+//  Created by Zaid Neurothrone on 2023-01-05.
+//
+
+extension Collection {
+  var isNotEmpty: Bool {
+    !self.isEmpty
+  }
+}

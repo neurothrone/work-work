cc220fbc53ec985fb91d6be9e9c1d43a55e8e079
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-01-06 00:19:09 +0100

    add CustomLinkView

---
+++ App/WorkWork/UI/Components/CustomLinkView.swift
@@ -0,0 +1,33 @@
+//
+//  CustomLinkView.swift
+//  WorkWork
+//
+//  Created by Zaid Neurothrone on 2023-01-05.
+//
+
+import SwiftUI
+
+struct CustomLinkView: View {
+  let urlString: String
+  let text: String
+  
+  var body: some View {
+    Link(destination: URL(string: urlString)!) {
+      HStack(alignment: .center) {
+        Text(text)
+          .font(.footnote)
+        Image(systemName: MyApp.SystemImage.linkCircleFill)
+      }
+      .foregroundColor(.blue)
+    }
+  }
+}
+
+struct CustomLinkView_Previews: PreviewProvider {
+  static var previews: some View {
+    CustomLinkView(
+      urlString: MyApp.Link.svgRepo,
+      text: "App icon by svgrepo.com"
+    )
+  }
+}

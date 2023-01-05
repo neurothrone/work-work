884f9d9e183a5c1c417a36acae5a40c41480a1c6
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-01-05 19:09:43 +0100

    move to another directory

--- App/WorkWork/ViewModels/TodosViewModel.swift
+++ App/WorkWork/ViewModels/TodosViewModel.swift
@@ -8,23 +8,35 @@
 import CoreData
 import SwiftUI
 
+//final class TodosViewModel: BaseViewModel<Todo> {
+//  let todoList: TodoList
+//  
+//  init(todoList: TodoList) {
+//    self.todoList = todoList
+//  }
+//
+//  override func createEntity(using context: NSManagedObjectContext) {
+//    Todo.create(with: title, in: todoList, using: context)
+//  }
+//}
+
 final class TodosViewModel: ObservableObject {
   enum TodoMode {
     case add,
          edit
   }
-  
+
   @Published var isTextFieldFocused: Bool = false
   @Published var todoTitle = ""
   @Published var selectedTodo: Todo? = nil
   @Published var activeTodoMode: TodoMode? = nil
-  
+
   let todoList: TodoList
-  
+
   init(todoList: TodoList) {
     self.todoList = todoList
   }
-  
+
   var activeModeSystemName: String {
     switch activeTodoMode {
     case .add:
@@ -35,7 +47,7 @@ final class TodosViewModel: ObservableObject {
       return "circle.slash"
     }
   }
-  
+
   func changeTodoMode() {
     withAnimation(.linear) {
       switch activeTodoMode {
@@ -52,42 +64,42 @@ final class TodosViewModel: ObservableObject {
       }
     }
   }
-  
+
   func changeViewToEditingTodo(_ todo: Todo) {
     activeTodoMode = .edit
     todoTitle = todo.title
     selectedTodo = todo
   }
-  
+
   func addOrUpdateTodo(using context: NSManagedObjectContext) {
     if let selectedTodo {
       selectedTodo.title = todoTitle
       selectedTodo.save(using: context)
-      
+
       withAnimation(.linear) {
         self.selectedTodo = nil
         activeTodoMode = nil
       }
     } else {
       Todo.create(with: todoTitle, in: todoList, using: context)
-      
+
       withAnimation(.linear) {
         isTextFieldFocused = true
       }
     }
-    
+
     withAnimation(.linear) {
       todoTitle = ""
     }
   }
-  
+
   func deleteTodo(_ todo: Todo, using context: NSManagedObjectContext) {
     withAnimation {
       todo.delete(using: context)
     }
-    
+
     guard activeTodoMode == .edit else { return }
-    
+
     withAnimation(.linear) {
       todoTitle = ""
       activeTodoMode = nil

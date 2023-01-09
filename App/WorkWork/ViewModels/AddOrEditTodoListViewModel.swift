06ada9a4339008b449c309e45e60c47f3d6ecb69
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-01-09 18:09:01 +0100

    add new view model responsible for adding or editing todo lists in sheet

---
+++ App/WorkWork/ViewModels/AddOrEditTodoListViewModel.swift
@@ -0,0 +1,43 @@
+//
+//  AddOrEditTodoListViewModel.swift
+//  WorkWork
+//
+//  Created by Zaid Neurothrone on 2023-01-09.
+//
+
+import CoreData
+import Foundation
+
+final class AddOrEditTodoListViewModel: ObservableObject {
+  @Published var title: String = ""
+  @Published var selectedIcon: Icon = .default
+  @Published var todoList: TodoList?
+  
+  let actionText: String
+  
+  init(todoList: TodoList? = nil) {
+    self.todoList = todoList
+    
+    if let todoListToUpdate = todoList {
+      actionText = "Update"
+      
+      title = todoListToUpdate.title
+      selectedIcon = Icon(rawValue: todoListToUpdate.systemImage) ?? .default
+    } else {
+      actionText = "Add"
+    }
+  }
+  
+  func addOrUpdate(using context: NSManagedObjectContext) {
+    if let todoListToUpdate = todoList {
+      TodoList.update(
+        list: todoListToUpdate,
+        with: title,
+        newIcon: selectedIcon,
+        using: context
+      )
+    } else {
+      _ = TodoList.create(with: title, icon: selectedIcon, using: context)
+    }
+  }
+}

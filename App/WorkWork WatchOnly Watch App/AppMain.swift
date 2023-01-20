b0bc63bf9b7d7396a979ca475571f8ef7af7ec66
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-01-20 16:12:52 +0100

    add apple watch target

---
+++ App/WorkWork WatchOnly Watch App/AppMain.swift
@@ -0,0 +1,17 @@
+//
+//  AppMain.swift
+//  WorkWork WatchOnly Watch App
+//
+//  Created by Zaid Neurothrone on 2023-01-20.
+//
+
+import SwiftUI
+
+@main
+struct AppMain: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}

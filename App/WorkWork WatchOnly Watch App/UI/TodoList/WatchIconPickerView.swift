9a3dcb0a15d674a2d608c42f2f1016300f504b03
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-01-21 00:42:03 +0100

    create an IconPickerView specialized for Watch app

---
+++ App/WorkWork WatchOnly Watch App/UI/TodoList/WatchIconPickerView.swift
@@ -0,0 +1,51 @@
+//
+//  WatchIconPickerView.swift
+//  WorkWork WatchOnly Watch App
+//
+//  Created by Zaid Neurothrone on 2023-01-21.
+//
+
+import SwiftUI
+
+struct WatchIconPickerView: View {
+  @Environment(\.dismiss) var dismiss
+  @EnvironmentObject var appState: AppState
+  
+  @ObservedObject var viewModel: AddOrEditTodoListViewModel
+  
+  private let columns = [
+    GridItem(
+      .adaptive(minimum: 60),
+      spacing: .zero,
+      alignment: .center
+    )
+  ]
+  
+  var body: some View {
+    ScrollView {
+      LazyVGrid(columns: columns) {
+        IconPickerView(
+          selectedIcon: $viewModel.selectedIcon,
+          selectionColor: appState.selectedColor.color
+        )
+      }
+    }
+    .navigationTitle("Select an icon")
+    .toolbar {
+      //MARK: Navigation Bar
+      ToolbarItem(placement: .confirmationAction) {
+        Button("Done", action: { dismiss() })
+          .tint(appState.selectedColor.color)
+      }
+    }
+  }
+}
+
+struct WatchIconPickerView_Previews: PreviewProvider {
+  static var previews: some View {
+    NavigationStack {
+      WatchIconPickerView(viewModel: AddOrEditTodoListViewModel(todoList: nil))
+        .environmentObject(AppState())
+    }
+  }
+}
